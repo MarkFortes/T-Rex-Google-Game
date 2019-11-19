5942c0d8dd1e3e97e04fe22caa362fdb0403577e
Marc Fortes <marcfortesbcn@gmail.com>
2019-11-19 23:44:49 +0100

    Primer commit

---
+++ script.js
@@ -0,0 +1,52 @@
+//Funcion que oye las teclas pulsadas
+document.addEventListener('keydown', function(evento){
+  if (evento.keyCode == 32 || evento.keyCode == 38) {
+    console.log("Salto");
+  }
+});
+
+
+var imgRex, imgNube, imgCactus, imgSuelo;
+
+function cargaImagenes(){
+  imgRex = new Image();
+  imgNube = new Image();
+  imgCactus = new Image();
+  imgSuelo = new Image();
+
+  imgRex.src = "img/rex.png";
+  imgNube.src = "img/nube.png";
+  imgCactus.src = "img/cactus.png";
+  imgSuelo.src = "img/suelo.png";
+}
+
+var ancho = 700;
+var alto = 300;
+var canvas,ctx;
+
+function init(){
+  canvas = document.getElementById('myCanvas');
+  ctx = canvas.getContext('2d');
+  cargaImagenes();
+}
+
+function borraCanvas(){
+  canvas.width = ancho;
+  canvas.height = alto;
+}
+
+function dibujaRex(){
+  ctx.drawImage(imgRex,100,100,50,50);
+}
+
+
+//BUCLE PRINCIPAL -------------------------------------------------------------
+var FPS = 10;
+setInterval(function(){
+  principal();
+},1000/10); //10 veces por segundo
+
+function principal(){
+  borraCanvas();
+  dibujaRex();
+}
